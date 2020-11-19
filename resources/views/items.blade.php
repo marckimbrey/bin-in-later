ba53b80d4d701f2a473fb7e468b6d3ab442917c1
marckimbrey <marckimbrey@gmail.com>
2020-11-19 09:43:19 +0000

    Added basic views for item resource routes

---
+++ resources/views/items.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <!-- display as table -->
+    @foreach($items as $item)
+        
+    @endforeach
+</div>
+@endsection
